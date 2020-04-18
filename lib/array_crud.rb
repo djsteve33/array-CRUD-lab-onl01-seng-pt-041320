84ad498b02cf36f5694b9ae0851a7c15d0965569
Steve Womble <swomble@aol.com>
2020-04-18 15:53:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,17 +1,21 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
-end
+  eighties_movies = ["Top Gun", "Footloose", "Sixteen Candles", "An Officer and A Gentleman"]
+  end
 
 def add_element_to_end_of_array(array, element)
-  
+  eighties_movies = ["Top Gun", "Footloose", "Sixteen Candles", "An Officer and A Gentleman"]
+  element = "arrays!"
+  eighties_movies << element
 end
 
 def add_element_to_start_of_array(array, element)
-  
+   eighties_movies = ["Top Gun", "Footloose", "Sixteen Candles", "An Officer and A Gentleman","arrays!"]
+   element = "wow"
+   eighties_movies.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
